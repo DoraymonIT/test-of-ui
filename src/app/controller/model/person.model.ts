6ac810d9d375b528376e6e6b9c6673faddf6c3db
DoraymonIT <53580861+DoraymonIT@users.noreply.github.com>
2021-03-16 21:02:29 +0100

    90% of sidebar2

---
+++ src/app/controller/model/person.model.ts
@@ -0,0 +1,7 @@
+export class Person{
+  id:number;
+  nom: string;
+  username:string;
+  photo:string;
+  status: 'follow' | 'unfollow';
+}
--- src/app/side-bar2/side-bar2.component.ts
+++ src/app/side-bar2/side-bar2.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from '../controller/model/person.model';
 
 @Component({
   selector: 'app-side-bar2',
   templateUrl: './side-bar2.component.html',
-  styleUrls: ['./side-bar2.component.scss']
+  styleUrls: ['./side-bar2.component.scss'],
 })
 export class SideBar2Component implements OnInit {
+  public persons: Array<Person> = [
+    {
+      id: 1,
+      nom: 'Sarah',
+      status: 'follow',
+      username: '@dr.sarah',
+      photo: '../../assets/images/profile1.jpg',
+    },
+    {
+      id: 2,
+      nom: 'Arthur',
+      status: 'unfollow',
+      username: '@shelby',
+      photo: '../../assets/images/profile2.jpg',
+    },
+    {
+      id: 3,
+      nom: 'Van D.',
+      status: 'follow',
+      username: '@vindiesel',
+      photo: '../../assets/images/profile3.jpg',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
