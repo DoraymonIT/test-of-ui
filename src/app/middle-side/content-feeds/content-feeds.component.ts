8709ac2b9b15cd2e3ba0bce72693d903674a9930
DoraymonIT <53580861+DoraymonIT@users.noreply.github.com>
2021-03-16 22:36:21 +0100

    just implementing flex instead of bootstrap left to do

--- src/app/middle-side/content-feeds/content-feeds.component.ts
+++ src/app/middle-side/content-feeds/content-feeds.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-content-feeds',
   templateUrl: './content-feeds.component.html',
-  styleUrls: ['./content-feeds.component.scss']
+  styleUrls: ['./content-feeds.component.scss'],
 })
 export class ContentFeedsComponent implements OnInit {
-
-  constructor() { }
+  number_of_likes: number;
+  constructor() {}
 
   ngOnInit(): void {
+    localStorage.setItem('likes', '0');
+    this.number_of_likes = parseInt(localStorage.getItem('likes'));
   }
-
+  addLikes() {
+    this.number_of_likes = this.number_of_likes + 1;
+    localStorage.setItem('likes', this.number_of_likes.toString());
+  }
 }
